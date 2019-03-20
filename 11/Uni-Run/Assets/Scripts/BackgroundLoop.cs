6b6d8e8257fe4260b6bcd2378e0b9f22ad81633b
오명섭 <oms1226@gmail.com>
2019-03-20 16:35:27 +0900

    unirun 배경 돌리기 작업 중

--- 11/Uni-Run/Assets/Scripts/BackgroundLoop.cs
+++ 11/Uni-Run/Assets/Scripts/BackgroundLoop.cs
@@ -6,14 +6,23 @@ public class BackgroundLoop : MonoBehaviour {
 
     private void Awake() {
         // 가로 길이를 측정하는 처리
+        // 자신의 박스 콜라이더에서 가로 길이를 찾아 width에 저장
+        BoxCollider2D boxCollider = GetComponent<BoxCollider2D>;
+        width = boxCollider.size.x;
     }
 
     private void Update() {
         // 현재 위치가 원점에서 왼쪽으로 width 이상 이동했을때 위치를 리셋
+        if(transform.position.x <= -width)
+        {
+            Reposition();
+        }
     }
 
     // 위치를 리셋하는 메서드
     private void Reposition() {
-        
+        //오른쪽 ( x방)으로 두 칸 밀기
+        Vector3 offset = new Vector3(width * 2, 0, 0);
+        transform.position = transform.position + offset;
     }
 }
--- 11/Uni-Run/Assets/Scripts/ScrollingObject.cs
+++ 11/Uni-Run/Assets/Scripts/ScrollingObject.cs
@@ -4,9 +4,15 @@
 public class ScrollingObject : MonoBehaviour {
     public float speed = 10f; // 이동 속도
 
+
     private void Update() {
         // 게임 오브젝트를 왼쪽으로 일정 속도로 평행 이동하는 처리
+        //소문자로 시작하는 transform 으, 자신의 게임 오브젝트의 트랜스폼 컴포넌트로 즉시 접근
         //transform.Translate(Vector3.left * speed);
-        transform.Translate(-speed, 0f, 0f);
+        // 1초 60번 * 한번에 10m = 1초에 600m
+        transform.Translate(-speed, 0f, 0f);//겁나 빨리 움직인다.
+        // 1초 60번 * 한번에 10m * 1/60 = 1초 10m
+        //Time.deltaTime = Update 와 Update 사이의 실행 간격 = 화면의 갱신 주기
+        //1번에 60초 주기는
     }
 }
