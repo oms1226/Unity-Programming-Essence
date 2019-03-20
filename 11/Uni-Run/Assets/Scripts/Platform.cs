098d26244b908411b5789b9e277007a39ca45fbd
오명섭 <oms1226@gmail.com>
2019-03-20 17:28:32 +0900

    unirun 장애물이 있는 platform 작업 완료

--- 11/Uni-Run/Assets/Scripts/Platform.cs
+++ 11/Uni-Run/Assets/Scripts/Platform.cs
@@ -6,11 +6,29 @@ public class Platform : MonoBehaviour {
     private bool stepped = false; // 플레이어 캐릭터가 밟았었는가
 
     // 컴포넌트가 활성화될때 마다 매번 실행되는 메서드
+    // start는 한번만 호출되지, 해당 함수는 비활성화해서 활성화 될때마다 호출된다 
     private void OnEnable() {
         // 발판을 리셋하는 처리
+        stepped = false;
+        foreach(GameObject obstacle in obstacles)
+        {
+            obstacle.SetActive(false);
+            //0 or 1
+            if(Random.Range(0,2) == 0)
+            {
+                //50% 확률로 현재 꺼낸 장애물이 활성화
+                obstacle.SetActive(true);
+            }
+        }
     }
 
     void OnCollisionEnter2D(Collision2D collision) {
+        if(stepped)
+        {
+            return;
+        }
         // 플레이어 캐릭터가 자신을 밟았을때 점수를 추가하는 처리
+        GameManager.instance.AddScore(1);
+        stepped = true;
     }
 }
